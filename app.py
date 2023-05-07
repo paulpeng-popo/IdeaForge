c61038d6a79e1ec88a529edfc1adc4c14d7511a5
paulpeng-popo <kmes1234@gmail.com>
2023-05-07 08:41:05 +0800

    [FEAT] TTS and repeat functions

--- app.py
+++ app.py
@@ -1,22 +1,62 @@
-import flask
+import os
+import sys
+import base64
 
-from flask import Flask, request, jsonify,render_template
+from flask_cors import CORS
+from toolspack.crossAPI import TTSCrossLanguage
+from toolspack.recogAPI import ASRChineseAPI
+from flask import Flask, request, jsonify, render_template
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+AudioPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "audio")
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def home():
      return render_template("index.html")
 
-@app.route('/api', methods=['GET'])
+
+@app.route("/say", methods=["POST"])
 def api():
-    return jsonify({'message': 'Hello, World!'})
+    text = request.form.get("text")
+    tts_client = TTSCrossLanguage(AudioPath)
+    tts_client.set_language(language="zh", speaker="UDN")
+    tts_client.askForService(text)
 
-@app.route('/api/sum', methods=['GET'])
-def sum():
-    a = request.args.get('a')
-    b = request.args.get('b')
-    return jsonify({'sum': int(a) + int(b)})
+    filename = os.path.join(AudioPath, "output.wav")
+    with open(filename, "rb") as audio:
+        audio_b64 = base64.b64encode(audio.read())
 
-if __name__ == '__main__':
+    result = {
+        "status": "true",
+        "data": audio_b64.decode("utf-8"),
+    }
+    return jsonify(result)
+
+
+@app.route("/chatroom", methods=["GET"])
+def chatroom():
+    return render_template("chatroom.html")
+
+
+@app.route("/speak", methods=["GET"])
+def speak():
+    return render_template("record.html")
+
+
+@app.route("/recog", methods=["POST"])
+def recog():
+    audio_blob = request.data
+    recog_client = ASRChineseAPI(audio_blob, AudioPath)
+    recog_client.recognize()
+    text = recog_client.get_text()
+    print(text)
+    return jsonify(text=text)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
